1348cbc3b4fa78f06205d518fc81b197bde89c21
AlexPoddubny <usatij.alex@gmail.com>
2019-10-22 12:14:23 +0300

    Cities game working

--- cities/func.php
+++ cities/func.php
@@ -1,4 +1,7 @@
 <?php
+	$cities = get_cities();
+	//var_dump($cities);
+	
 	function get_cities(){
 		$url = "https://uk.wikipedia.org/wiki/%D0%9C%D1%96%D1%81%D1%82%D0%B0_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B8_(%D0%B7%D0%B0_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D1%96%D1%82%D0%BE%D0%BC)";
 		$page = file_get_contents($url);
@@ -9,13 +12,47 @@
 			$page = substr($page, strpos($page, $key) + strlen($key), strlen($page));
 			$key = "title=\"";
 			$page = substr($page, strpos($page, $key) + strlen($key), strlen($page));
-			$cities[] = substr($page, 0, strpos($page, '"'));
+			$city = substr($page, 0, strpos($page, '"'));
+			//removing ()
+			if (strpos($city, "(") > 0){
+				$city = stristr($city, " ", true);
+			}
+			$cities[] = $city;
 		} while (strpos($page, '</tr>') > 0);
 		array_pop($cities);
 		return $cities;
 	}
 	
+	function detect_encode($city){
+		if (mb_detect_encoding($city) == "UTF-8"){
+			$num = 2;
+		} else {
+			$num = 1;
+		}
+		return $num;
+	}
+	
 	function get_last_letter($city){
-		return substr($city, -1);
+		return substr($city, - detect_encode($city));
+	}
+	
+	function get_first_letter($city){
+		return mb_strtolower(substr($city, 0, detect_encode($city)));
+	}
+	
+	function get_city($letter){
+		global $cities;
+		foreach ($cities as $city){
+			if ($letter == get_first_letter($city)){
+				return $city;
+			}
+		}
+		return null;
+	}
+	
+	function responce($city){
+		if (isset($city)) {
+			return get_city(get_last_letter($city));
+		}
 	}
 ?>
--- cities/index.php
+++ cities/index.php
@@ -1,13 +1,18 @@
 <?php
 	require __DIR__ . "/func.php";
-	$cities = get_cities();
 ?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Cities Game</title>
 </head>
 <body>
 	<h1>Сыграем в города?</h1>
+	<?php
+		if (isset($_POST['city'])){
+			echo "<p>Мой ответ: " . responce($_POST['city']) . "</p>";
+		}
+	?>
 	<label>Введите город:</label>
 	<form action="/cities/index.php" method="post">
 		<input type="text" name="city" value="<?php if (isset($_POST['city'])){ echo $_POST['city']; }; ?>">
