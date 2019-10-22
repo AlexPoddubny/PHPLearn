02d2e676172281ebf021bd1c15acb3bb2efd083d
AlexPoddubny <usatij.alex@gmail.com>
2019-10-23 00:48:42 +0300

    Lesson 5

---
+++ lesson5/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>
+	
+	</title>
+</head>
+<body>
+<?php
+	class Table
+	{
+		public $color;
+		public $legs;
+	}
+	$table1 = new Table();
+	var_dump($table1);
+?>
+</body>
+</html>
